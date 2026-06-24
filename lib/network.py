92c38b030274c0739c4335fdb6ff7a99b52fc2b0
root <root@opencode.fritz.box>
2026-06-24 09:57:45 +0000

    Send address as second param to scripthash.subscribe

--- lib/network.py
+++ lib/network.py
@@ -640,12 +640,9 @@ class Network(util.DaemonThread):
         return cb2
 
     def subscribe_to_addresses(self, addresses, callback):
-        # Register addresses on server first (auto-populates ADDRESS_MAP)
-        reg_msgs = [('blockchain.address.subscribe', [addr]) for addr in addresses]
-        self.send(reg_msgs, lambda x: None)
         hashes = [self.addr_to_scripthash(addr) for addr in addresses]
-        sub_msgs = [('blockchain.scripthash.subscribe', [x]) for x in hashes]
-        self.send(sub_msgs, self.overload_cb(callback))
+        msgs = [('blockchain.scripthash.subscribe', [h, addr]) for h, addr in zip(hashes, addresses)]
+        self.send(msgs, self.overload_cb(callback))
 
     def request_address_history(self, address, callback):
         h = self.addr_to_scripthash(address)
